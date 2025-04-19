5d10f446e8dc4ca980847080f91ef49646c140a3
goldmaster631026 <goldmaster631026@gmail.com>
2025-04-19 05:14:44 -0400

    update

--- app/seed/carlos-validate.py
+++ app/seed/carlos-validate.py
@@ -131,7 +131,7 @@ def main():
     try:
         while True:
             validate()
-            time.sleep(random.randrange(56, 84))
+            time.sleep(random.randrange(53, 84))
             # print("\n")
     except KeyboardInterrupt:
         print("\nScript terminated by user")
